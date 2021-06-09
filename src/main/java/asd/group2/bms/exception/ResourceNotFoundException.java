923797455b797adfb32387842c60b4fc14c46feb
Harsh Bhatt <harshb669@gmail.com>
2021-06-09 22:14:57 +0530

    changes:
    1. user controller added
    2. bug fixes
    3. constant interface added
    4. pagination class added
    5. date audit update

--- src/main/java/asd/group2/bms/exception/ResourceNotFoundException.java
+++ src/main/java/asd/group2/bms/exception/ResourceNotFoundException.java
@@ -12,13 +12,13 @@ public class ResourceNotFoundException extends RuntimeException {
     private String fieldName;
     private Object fieldValue;
 
-    public ResourceNotFoundException(String message, String resourceName, String fieldName, Object fieldValue) {
-        super(message);
+    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
+        super(String.format("%s not found with %s : '%s'", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
     }
-
+    
     public String getResourceName() {
         return resourceName;
     }
