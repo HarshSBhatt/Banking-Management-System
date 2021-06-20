4d505f690c906820336c117cc27aa9d581c44db2
Harsh Bhatt <harshb669@gmail.com>
2021-06-20 17:37:44 +0530

    Service to reset password done

--- src/main/java/asd/group2/bms/service/UserService.java
+++ src/main/java/asd/group2/bms/service/UserService.java
@@ -7,10 +7,10 @@ import asd.group2.bms.payload.response.UserProfile;
 import asd.group2.bms.repository.UserRepository;
 import asd.group2.bms.security.UserPrincipal;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
-
-import java.util.Optional;
 
 @Service
 public class UserService {
@@ -30,20 +30,38 @@ public class UserService {
 
     public ApiResponse changePassword(String oldPassword, String newPassword, UserPrincipal currentUser) {
         if (passwordEncoder.matches(oldPassword, currentUser.getPassword())) {
-            Optional<User> user = getUserByEmail(currentUser.getEmail());
-            if (user.isPresent()) {
-                User updatedUser = user.get();
-                updatedUser.setPassword(passwordEncoder.encode(newPassword));
-                userRepository.save(updatedUser);
-            }
+            User user = getUserByEmail(currentUser.getEmail());
+            user.setPassword(passwordEncoder.encode(newPassword));
+            userRepository.save(user);
             return new ApiResponse(true, "Password changed successfully!");
         } else {
             return new ApiResponse(false, "Old password is wrong!");
         }
     }
 
-    public Optional<User> getUserByEmail(String email) {
-        return userRepository.findByEmail(email);
+    public ResponseEntity<?> resetPassword(String newPassword, String confirmNewPassword, User user) {
+        if (newPassword.equals(confirmNewPassword)) {
+            user.setPassword(passwordEncoder.encode(newPassword));
+            user.setForgotPasswordToken(null);
+            userRepository.save(user);
+            return ResponseEntity.ok(new ApiResponse(true, "Password reset successfully"));
+        } else {
+            return new ResponseEntity<>(new ApiResponse(false, "New passwords are not same"), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    public void updateResetForgotPasswordToken(String token, String email) {
+        User user = getUserByEmail(email);
+        user.setForgotPasswordToken(token);
+        userRepository.save(user);
+    }
+
+    public User getUserByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new ResourceNotFoundException("Email", "email", email));
+    }
+
+    public User getUserByToken(String token) {
+        return userRepository.findByForgotPasswordToken(token).orElseThrow(() -> new ResourceNotFoundException("Reset Password Token", "token", token));
     }
 
     public UserProfile getUserProfileByUsername(String username) {
