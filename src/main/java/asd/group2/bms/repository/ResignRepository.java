9f80dcc1858d2cb53e69f9a54a81351b147c739d
Aditya <ad658014@dal.ca>
2021-06-24 13:03:10 +0530

    Created resign repository for entity resigns

--- src/main/java/asd/group2/bms/repository/ResignRepository.java
+++ src/main/java/asd/group2/bms/repository/ResignRepository.java
@@ -1,4 +1,8 @@
 package asd.group2.bms.repository;
 
-public class ResignRepository {
+import asd.group2.bms.model.resign.ResignRequest;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ResignRepository extends JpaRepository<ResignRequest, Long> {
+
 }
