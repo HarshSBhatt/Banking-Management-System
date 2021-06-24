de4c56e888aeba91e366a283f9404984f8d84bd6
Aditya <ad658014@dal.ca>
2021-06-24 13:06:21 +0530

    Created resign service

--- src/main/java/asd/group2/bms/service/ResignService.java
+++ src/main/java/asd/group2/bms/service/ResignService.java
@@ -1,4 +1,26 @@
 package asd.group2.bms.service;
 
+import asd.group2.bms.model.resign.RequestStatus;
+import asd.group2.bms.model.resign.ResignRequest;
+import asd.group2.bms.model.user.User;
+import asd.group2.bms.payload.response.ApiResponse;
+import asd.group2.bms.repository.ResignRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Date;
+
+@Service
 public class ResignService {
+    @Autowired
+    ResignRepository resignRepository;
+
+
+    public ApiResponse resign(User user, Date date, String reason){
+        ResignRequest resignRequest = new ResignRequest(user, date, reason, RequestStatus.PENDING);
+        resignRepository.save(resignRequest);
+
+
+        return new ApiResponse(true, "Resignation request submitted successfully!");
+    }
 }
