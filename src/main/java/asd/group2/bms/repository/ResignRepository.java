4c4e037f07067b31946124d206a3556dbe9809b7
Aditya <ad658014@dal.ca>
2021-06-25 19:34:15 +0530

    Added method to fetch resign list by status in repo

--- src/main/java/asd/group2/bms/repository/ResignRepository.java
+++ src/main/java/asd/group2/bms/repository/ResignRepository.java
@@ -1,8 +1,18 @@
 package asd.group2.bms.repository;
 
+import asd.group2.bms.model.resign.RequestStatus;
 import asd.group2.bms.model.resign.ResignRequest;
+import asd.group2.bms.model.user.User;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ResignRepository extends JpaRepository<ResignRequest, Long> {
+import java.util.Optional;
 
+public interface ResignRepository extends JpaRepository<ResignRequest, Long> {
+    /**
+     * @param requestStatus: request status
+     * @descriptions: This will return the resign records by status.
+     */
+    Page<ResignRequest> findByRequestStatusEquals(RequestStatus requestStatus, Pageable pageable);
 }
