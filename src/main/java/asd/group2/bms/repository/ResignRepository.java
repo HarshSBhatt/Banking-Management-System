9cc360e9f61f09a1028b029f93b640ca7bb1eb1a
Aditya <ad658014@dal.ca>
2021-06-25 19:27:28 +0530

    Revert "Added method for resign in ModelMapper class"
    This reverts commit c56413b9

--- src/main/java/asd/group2/bms/repository/ResignRepository.java
+++ src/main/java/asd/group2/bms/repository/ResignRepository.java
@@ -1,18 +1,8 @@
 package asd.group2.bms.repository;
 
-import asd.group2.bms.model.resign.RequestStatus;
 import asd.group2.bms.model.resign.ResignRequest;
-import asd.group2.bms.model.user.User;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.Optional;
-
 public interface ResignRepository extends JpaRepository<ResignRequest, Long> {
-    /**
-     * @param requestStatus: request status
-     * @descriptions: This will return the resign records by status.
-     */
-    Page<ResignRequest> findByRequestStatusEquals(RequestStatus requestStatus, Pageable pageable);
+
 }
