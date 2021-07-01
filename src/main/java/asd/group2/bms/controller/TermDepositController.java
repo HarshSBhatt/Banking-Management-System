4069ab80d1b38ffbc7a16d567b3de94b6b0837c0
Aditya <ad658014@dal.ca>
2021-07-01 14:10:35 +0530

    Created TermDepositController

---
+++ src/main/java/asd/group2/bms/controller/TermDepositController.java
@@ -0,0 +1,22 @@
+package asd.group2.bms.controller;
+
+import asd.group2.bms.model.term_deposit.TermDeposit;
+import asd.group2.bms.service.TermDepositService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api")
+public class TermDepositController {
+    @Autowired
+    TermDepositService termDepositService;
+
+    @GetMapping("/termdeposit")
+    public List<TermDeposit> getTermDeposit() {
+        return termDepositService.getTermDeposit();
+    }
+}
