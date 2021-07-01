b14533f61237c33ecd204ce69f720f2df1af8d9e
Aditya <ad658014@dal.ca>
2021-07-01 14:11:28 +0530

    Added termdeposit in permitALl

--- src/main/java/asd/group2/bms/config/SecurityConfigurer.java
+++ src/main/java/asd/group2/bms/config/SecurityConfigurer.java
@@ -61,7 +61,7 @@ public class SecurityConfigurer extends WebSecurityConfigurerAdapter {
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and().authorizeRequests()
                 .antMatchers("/", "/favicon.ico", "/**/*.png", "/**/*.gif", "/**/*.svg", "/**/*.jpg", "/**/*.html",
                         "/**/*.css", "/**/*.js")
-                .permitAll().antMatchers("/api/auth/**", "/api/forgot-password", "/api/reset-password").permitAll()
+                .permitAll().antMatchers("/api/auth/**", "/api/forgot-password", "/api/reset-password", "/api/termdeposit").permitAll()
                 .antMatchers("/api/user/checkUsernameAvailability", "/api/user/checkEmailAvailability").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/bank/**", "/api/users/**", "/api/account/**", "/api/staff/**").permitAll().anyRequest().authenticated();
 
