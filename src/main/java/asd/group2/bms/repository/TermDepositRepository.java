a13416d3d10c278ce3ec3b2f4a10e62c8304438d
Aditya <ad658014@dal.ca>
2021-07-01 14:05:00 +0530

    Created TermDepositRepository

---
+++ src/main/java/asd/group2/bms/repository/TermDepositRepository.java
@@ -0,0 +1,11 @@
+package asd.group2.bms.repository;
+
+import asd.group2.bms.model.term_deposit.TermDeposit;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface TermDepositRepository extends JpaRepository<TermDeposit, Long> {
+}
