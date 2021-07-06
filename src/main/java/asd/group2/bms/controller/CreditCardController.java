5d1d6a64167a0e737943b1ac9f0b5acf5aa40dc5
Aditya <ad658014@dal.ca>
2021-07-06 13:34:10 +0530

    Added method updateCreditCardRequestStatus

--- src/main/java/asd/group2/bms/controller/CreditCardController.java
+++ src/main/java/asd/group2/bms/controller/CreditCardController.java
@@ -1,17 +1,21 @@
 package asd.group2.bms.controller;
 
+import asd.group2.bms.model.cards.credit.CreditCard;
 import asd.group2.bms.model.cards.credit.CreditCardStatus;
+import asd.group2.bms.payload.request.UpdateCreditCardStatusRequest;
+import asd.group2.bms.payload.request.UpdateResignStatusRequest;
+import asd.group2.bms.payload.response.ApiResponse;
 import asd.group2.bms.payload.response.CreditCardListResponse;
 import asd.group2.bms.payload.response.PagedResponse;
 import asd.group2.bms.service.CreditCardService;
 import asd.group2.bms.util.AppConstants;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.security.RolesAllowed;
+import javax.validation.Valid;
 
 @RestController
 @RequestMapping("/api")
@@ -30,5 +34,18 @@ public class CreditCardController {
             @RequestParam(value = "page", defaultValue = AppConstants.DEFAULT_PAGE_NUMBER) int page,
             @RequestParam(value = "size", defaultValue = AppConstants.DEFAULT_PAGE_SIZE) int size) {
         return creditCardService.getCreditCardListByStatus(creditCardStatus, page, size);
+    }
+
+    @PutMapping("/services/creditcards")
+    @RolesAllowed({"ROLE_MANAGER", "ROLE_EMPLOYEE"})
+    public ResponseEntity<?> updateCreditCardRequestStatus(
+            @Valid @RequestBody UpdateCreditCardStatusRequest updateCreditCardStatusRequest){
+        CreditCard creditCard = creditCardService.setCreditCardRequestStatus(updateCreditCardStatusRequest.getCreditCardNumber(), updateCreditCardStatusRequest.getCreditCardStatus());
+        if (creditCard != null) {
+            return ResponseEntity.ok(new ApiResponse(true, "Credit Card request status changed successfully!"));
+        } else {
+            return new ResponseEntity<>(new ApiResponse(false, "Something went wrong while changing Credit Card request status!"),
+                    HttpStatus.BAD_REQUEST);
+        }
     }
 }
