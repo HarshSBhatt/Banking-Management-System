e47e897e2b894fc9f3baef2f7f6d1497ef259507
Aditya <ad658014@dal.ca>
2021-07-06 13:32:25 +0530

    Created CreditCard Status request

---
+++ src/main/java/asd/group2/bms/payload/request/UpdateCreditCardStatusRequest.java
@@ -0,0 +1,31 @@
+package asd.group2.bms.payload.request;
+
+import asd.group2.bms.model.cards.credit.CreditCardStatus;
+
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.NotNull;
+
+public class UpdateCreditCardStatusRequest {
+    @NotNull(message = "Credit Card Number is required")
+    private Long creditCardNumber;
+
+    @Enumerated(EnumType.STRING)
+    private CreditCardStatus creditCardStatus;
+
+    public Long getCreditCardNumber() {
+        return creditCardNumber;
+    }
+
+    public void setCreditCardNumber(Long creditCardNumber) {
+        this.creditCardNumber = creditCardNumber;
+    }
+
+    public CreditCardStatus getCreditCardStatus() {
+        return creditCardStatus;
+    }
+
+    public void setCreditCardStatus(CreditCardStatus creditCardStatus) {
+        this.creditCardStatus = creditCardStatus;
+    }
+}
