45404aaf936ac4cbb7b8a73c62f6b07513eef224
Geetanjali Bommera <gt584369@dal.ca>
2021-07-09 11:48:38 -0300

    Implemented interface

--- src/main/java/asd/group2/bms/service/DebitCardService.java
+++ src/main/java/asd/group2/bms/service/DebitCardService.java
@@ -1,34 +1,17 @@
 package asd.group2.bms.service;
 
-import asd.group2.bms.exception.ResourceNotFoundException;
 import asd.group2.bms.model.account.Account;
 import asd.group2.bms.model.cards.debit.DebitCard;
 
 public interface DebitCardService {
 
-  DebitCard createDebitCard(Account account);
+    DebitCard createDebitCard(Account account);
 
-  /**
-   *
-   * @param debitCardNumber: Debit Card Number
-   * @return Returns Debit card of the to change transaction limit
-   */
-  public DebitCard getDebitCardByNumber(Long debitCardNumber) {
-    return debitCardRepository.findById(debitCardNumber).orElseThrow(() -> new ResourceNotFoundException("DebitCard Number", "debitCardNumber", debitCardNumber));
-  }
+    DebitCard getDebitCardByNumber(Long debitCardNumber);
 
-  /**
-   *
-   * @param debitCardNumber: Debit Card Number
-   * @param transactionLimit: Transaction limit to set for the given debit card number
-   * @return Returns Debit card of the changed transaction limit
-   */
-  public DebitCard setDebitCardLimit(Long debitCardNumber, Integer transactionLimit) {
-    DebitCard debitCard = getDebitCardByNumber(debitCardNumber);
+    DebitCard setDebitCardLimit(Long debitCardNumber, Integer transactionLimit);
 
-    debitCard.setTransactionLimit(transactionLimit);
-    return debitCardRepository.save(debitCard);
-  }
+    DebitCard setDebitCardPin(Long debitCardNumber, String pin);
 
 
 }
