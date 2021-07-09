d15ff641e0be809600d1a5981215e8fbfbbbdf4c
Aditya <ad658014@dal.ca>
2021-07-09 13:41:01 +0530

    Implemented findById

--- src/main/java/asd/group2/bms/repositoryImpl/ResignRepositoryImpl.java
+++ src/main/java/asd/group2/bms/repositoryImpl/ResignRepositoryImpl.java
@@ -3,30 +3,79 @@ package asd.group2.bms.repositoryImpl;
 import asd.group2.bms.model.resign.RequestStatus;
 import asd.group2.bms.model.resign.ResignRequest;
 import asd.group2.bms.model.user.User;
+import asd.group2.bms.repository.ResignRepository;
+import asd.group2.bms.repositoryMapper.ResignRowMapper;
+import asd.group2.bms.repositoryMapper.UserRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
+import org.springframework.stereotype.Repository;
 
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 import java.util.List;
+import java.util.Optional;
 
-public interface ResignRepositoryImpl extends JpaRepository<ResignRequest, Long> {
+@Repository
+public class ResignRepositoryImpl extends JdbcDaoSupport implements ResignRepository {
 
-  /**
-   * @param requestStatus: request status
-   * @return This will return the resign records by status.
-   */
-  Page<ResignRequest> findByRequestStatusEquals(RequestStatus requestStatus, Pageable pageable);
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
 
-  /**
-   * @param userId: Id of user
-   * @return This will return the resign records by user id.
-   */
-  List<ResignRequest> findByUser_Id(Long userId);
+  @Autowired
+  DataSource dataSource;
 
-  /**
-   * @param user: User model object
-   * @return This will return the resign records by user.
-   */
-  List<ResignRequest> findByUserOrderByCreatedAtDesc(User user);
+  @Autowired
+  ResignRepositoryImpl resignRepository;
+
+  @PostConstruct
+  private void initialize() {
+    setDataSource(dataSource);
+  }
+
+  @Override
+  public Page<ResignRequest> findByRequestStatusEquals(RequestStatus requestStatus, Pageable pageable) {
+    return null;
+  }
+
+  @Override
+  public List<ResignRequest> findByUser_Id(Long userId) {
+    return null;
+  }
+
+  @Override
+  public Optional<ResignRequest> findById(Long resignId) {
+    String sql = "SELECT * FROM resigns r INNER JOIN users u ON r.user_id = u.id INNER JOIN roles r ON r.id = ur.role_id WHERE r.id = ?";
+    try {
+      return Optional.ofNullable(jdbcTemplate.queryForObject(sql, new Object[]{resignId},
+          new ResignRowMapper()));
+    } catch (EmptyResultDataAccessException e) {
+      return Optional.empty();
+    }
+  }
+
+  @Override
+  public List<ResignRequest> findByUserOrderByCreatedAtDesc(User user) {
+    return null;
+  }
+
+  @Override
+  public ResignRequest save(ResignRequest resignRequest) {
+    return null;
+  }
+
+  @Override
+  public Boolean update(ResignRequest resignRequest) {
+    return null;
+  }
+
+  @Override
+  public void delete(Long resignId) {
+
+  }
 
 }
