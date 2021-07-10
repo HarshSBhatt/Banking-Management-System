4f3b7fbd603d866dd8112057b585b25f72a54be8
Arpan Nayankumar Bhatt <ar205025@dal.ca>
2021-07-10 13:00:32 +0530

    TermDepositRepositoryImpl init commit

--- src/main/java/asd/group2/bms/repositoryImpl/TermDepositDetailRepositoryImpl.java
+++ src/main/java/asd/group2/bms/repositoryImpl/TermDepositDetailRepositoryImpl.java
@@ -2,19 +2,65 @@ package asd.group2.bms.repositoryImpl;
 
 import asd.group2.bms.model.term_deposit.TermDepositDetail;
 
+import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
+import asd.group2.bms.repository.TermDepositDetailRepository;
+import asd.group2.bms.repositoryMapper.RoleRowMapper;
+import asd.group2.bms.repositoryMapper.TermDepositDetailRowMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.support.JdbcDaoSupport;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.annotation.PostConstruct;
+import javax.sql.DataSource;
 
 
 @Repository
-public interface TermDepositDetailRepositoryImpl extends JpaRepository<TermDepositDetail, Long> {
+public class TermDepositDetailRepositoryImpl extends JdbcDaoSupport implements TermDepositDetailRepository {
+
+  @Autowired
+  private JdbcTemplate jdbcTemplate;
+
+  @Autowired
+  DataSource dataSource;
+
+  @PostConstruct
+  private void initialize() {
+    setDataSource(dataSource);
+  }
 
   /**
    * @param accountNumber: bank account number of the user
    * @return This will return the user's bank account number.
    */
-  List<TermDepositDetail> findTermDepositDetailByAccount_AccountNumber(Long accountNumber);
+  public List<TermDepositDetail> findTermDepositDetailByAccount_AccountNumber(Long accountNumber){
+    return new ArrayList<>();
+  }
+
+  @Override
+  public Optional<TermDepositDetail> findById(Long termDepositId) {
+    String sql = "SELECT * FROM term_deposit_details WHERE id = ?";
+    try {
+      return Optional.ofNullable(jdbcTemplate.queryForObject(sql,
+          new Object[]{termDepositId},
+          new TermDepositDetailRowMapper()));
+    } catch (EmptyResultDataAccessException e) {
+      return Optional.empty();
+    }
+  }
+
+  @Override
+  public TermDepositDetail save(TermDepositDetail termDepositDetail) {
+    return null;
+  }
+
+  @Override
+  public Boolean update(TermDepositDetail termDepositDetail) {
+    return null;
+  }
 
 }
--- src/main/java/asd/group2/bms/repositoryMapper/TermDepositDetailRowMapper.java
+++ src/main/java/asd/group2/bms/repositoryMapper/TermDepositDetailRowMapper.java
@@ -1,16 +1,41 @@
 package asd.group2.bms.repositoryMapper;
 
 import asd.group2.bms.model.term_deposit.TermDepositDetail;
+import asd.group2.bms.model.term_deposit.TermDepositStatus;
 import org.springframework.jdbc.core.RowMapper;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.ZoneOffset;
 
 public class TermDepositDetailRowMapper implements RowMapper<TermDepositDetail> {
 
   @Override
   public TermDepositDetail mapRow(ResultSet resultSet, int i) throws SQLException {
-    return null;
+    TermDepositDetail termDepositDetail = new TermDepositDetail();
+    UserRowMapper userRowMapper = new UserRowMapper();
+
+    LocalDate createdAt = resultSet.getDate("created_at").toLocalDate();
+    LocalDate updatedAt = resultSet.getDate("updated_at").toLocalDate();
+
+    termDepositDetail.setCreatedAt(createdAt.atStartOfDay(ZoneOffset.UTC).toInstant());
+    termDepositDetail.setUpdatedAt(updatedAt.atStartOfDay(ZoneOffset.UTC).toInstant());
+    termDepositDetail.setTermDepositId(resultSet.getLong("term_deposit_id"));
+    termDepositDetail.setTermDepositStatus(TermDepositStatus.valueOf(resultSet.getString(
+        "term_deposit_status")));
+
+
+    resignRequest.setResignId(resultSet.getLong("resign_id"));
+    resignRequest.setDate(resultSet.getDate("date"));
+    resignRequest.setReason(resultSet.getString("reason"));
+    resignRequest.setRequestStatus(RequestStatus.valueOf(resultSet.getString("request_status")));
+
+    User user = userRowMapper.mapRow(resultSet, i);
+    resignRequest.setUser(user);
+
+    return resignRequest;
+
   }
 
 }
