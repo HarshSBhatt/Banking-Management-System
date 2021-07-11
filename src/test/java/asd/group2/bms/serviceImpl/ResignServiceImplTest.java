9a96fef533d068aa9d78c3bd18437412223941a1
Aditya <ad658014@dal.ca>
2021-07-11 16:02:11 +0530

    created structure for ResignServiceImplTest

---
+++ src/test/java/asd/group2/bms/serviceImpl/ResignServiceImplTest.java
@@ -0,0 +1,9 @@
+package asd.group2.bms.serviceImpl;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class ResignServiceImplTest {
+
+}
