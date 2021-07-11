4ad5a1821de7ac0176399b1a5cd6c835e5f86359
Aditya <ad658014@dal.ca>
2021-07-11 16:31:36 +0530

    implemented getResignListByStatusTest

--- src/test/java/asd/group2/bms/serviceImpl/ResignServiceImplTest.java
+++ src/test/java/asd/group2/bms/serviceImpl/ResignServiceImplTest.java
@@ -1,9 +1,82 @@
 package asd.group2.bms.serviceImpl;
 
+import asd.group2.bms.model.resign.RequestStatus;
+import asd.group2.bms.model.resign.ResignRequest;
+import asd.group2.bms.model.user.AccountStatus;
+import asd.group2.bms.model.user.User;
+import asd.group2.bms.payload.response.PagedResponse;
+import asd.group2.bms.payload.response.ResignListResponse;
+import asd.group2.bms.repositoryImpl.ResignRepositoryImpl;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.domain.*;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 public class ResignServiceImplTest {
 
+  @Mock
+  ResignRepositoryImpl resignRepository;
+
+  @InjectMocks
+  ResignServiceImpl resignService;
+
+  @BeforeEach
+  public void setup() {
+    HttpServletRequest mockRequest = new MockHttpServletRequest();
+    ServletRequestAttributes servletRequestAttributes = new ServletRequestAttributes(mockRequest);
+    RequestContextHolder.setRequestAttributes(servletRequestAttributes);
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @AfterEach
+  public void teardown() {
+    RequestContextHolder.resetRequestAttributes();
+  }
+
+  @Test
+  void getResignListByStatusTest() {
+    String username = "aditya";
+    RequestStatus requestStatus = RequestStatus.PENDING;
+    int page = 0;
+    int size = 3;
+
+    Pageable pageable = PageRequest.of(page, size, Sort.Direction.ASC, "createdAt");
+
+    User user = new User();
+    user.setUsername("aditya");
+
+    ResignRequest resignRequest = new ResignRequest();
+    resignRequest.setUser(user);
+
+    List<ResignRequest> resigns = new ArrayList<>();
+    resigns.add(resignRequest);
+
+    Page<ResignRequest> pagedResigns = new PageImpl<>(resigns);
+
+    when(resignRepository.findByRequestStatusEquals(requestStatus, pageable)).thenReturn(pagedResigns);
+
+    PagedResponse<ResignListResponse> resignations =
+        resignService.getResignListByStatus(requestStatus, page, size);
+
+    assertEquals(username,
+        resignations.getContent().get(0).getUserMetaResponse().getUsername(),
+        "Wrong " +
+            "user was returned");
+  }
 }
