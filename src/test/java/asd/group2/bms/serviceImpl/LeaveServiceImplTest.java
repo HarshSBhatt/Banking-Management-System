7f334f63ab72eb07823391e45f5ed3a43a4c661d
Shivam Miteshkumar Barot <sh895418@dal.ca>
2021-07-12 12:46:03 +0530

    initialize testcases for leave service

--- src/test/java/asd/group2/bms/serviceImpl/LeaveServiceImplTest.java
+++ src/test/java/asd/group2/bms/serviceImpl/LeaveServiceImplTest.java
@@ -1,51 +1,70 @@
 package asd.group2.bms.serviceImpl;
 
 import asd.group2.bms.repositoryImpl.LeaveRepositoryImpl;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+import javax.servlet.http.HttpServletRequest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(MockitoExtension.class)
 class LeaveServiceImplTest {
 
-  @MockBean
-  private LeaveRepositoryImpl leaveRepository;
+  @Mock
+  LeaveRepositoryImpl leaveRepository;
 
   @InjectMocks
-  private LeaveServiceImpl leaveService;
+  LeaveServiceImpl leaveService;
 
   @BeforeEach
   void setUp() {
+    HttpServletRequest mockRequest = new MockHttpServletRequest();
+    ServletRequestAttributes servletRequestAttributes = new ServletRequestAttributes(mockRequest);
+    RequestContextHolder.setRequestAttributes(servletRequestAttributes);
+    MockitoAnnotations.openMocks(this);
+  }
+
+  @AfterEach
+  public void teardown() {
+    RequestContextHolder.resetRequestAttributes();
+  }
+
+
+  @Test
+  void getLeavesByStatusTest() {
+
   }
 
   @Test
-  void getLeavesByStatus() {
-
+  void getLeaveListByUserIdTest() {
   }
 
   @Test
-  void getLeaveListByUserId() {
+  void getLeaveByIdTest() {
   }
 
   @Test
-  void getLeaveById() {
+  void setLeaveRequestStatusTest() {
   }
 
   @Test
-  void setLeaveRequestStatus() {
+  void deleteLeaveRequestByIdTest() {
   }
 
   @Test
-  void deleteLeaveRequestById() {
-  }
+  void makeLeaveRequestTest() {
 
-  @Test
-  void makeLeaveRequest() {
   }
 
 }
