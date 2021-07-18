dbf18b3c34ffe4a36a542dca8df5355d16f28170
Geetanjali Bommera <gt584369@dal.ca>
2021-07-18 15:00:43 -0300

    Optimized imports

--- src/test/java/asd/group2/bms/serviceImpl/DebitCardServiceImplTest.java
+++ src/test/java/asd/group2/bms/serviceImpl/DebitCardServiceImplTest.java
@@ -17,8 +17,7 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
-import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class DebitCardServiceImplTest {
